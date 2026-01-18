6e60be926fb834351ceca384e56f3063d9d2ca4f
Aleksander Ziętkiewicz <227445@student.uek.krakow.pl>
2026-01-18 18:35:02 +0100

    ADB3

---
+++ ZaawansowaneBazyDanych/dbo/Tables/Customer.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerID]   INT                                         NOT NULL,
+    [NameStyle]    BIT                                         NOT NULL,
+    [Title]        NVARCHAR (8)                                NULL,
+    [FirstName]    NVARCHAR (50)                               NOT NULL,
+    [MiddleName]   NVARCHAR (50)                               NULL,
+    [LastName]     NVARCHAR (50)                               NOT NULL,
+    [Suffix]       NVARCHAR (10)                               NULL,
+    [CompanyName]  NVARCHAR (128)                              NULL,
+    [SalesPerson]  NVARCHAR (256)                              NULL,
+    [EmailAddress] NVARCHAR (50)                               NULL,
+    [Phone]        NVARCHAR (25)                               NULL,
+    [PasswordHash] VARCHAR (128)                               NOT NULL,
+    [PasswordSalt] VARCHAR (10)                                NOT NULL,
+    [rowguid]      UNIQUEIDENTIFIER                            DEFAULT (newid()) NOT NULL,
+    [ModifiedDate] DATETIME                                    DEFAULT (getdate()) NOT NULL,
+    [SysStartTime] DATETIME2 (7) GENERATED ALWAYS AS ROW START NOT NULL,
+    [SysEndTime]   DATETIME2 (7) GENERATED ALWAYS AS ROW END   NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC),
+    PERIOD FOR SYSTEM_TIME ([SysStartTime], [SysEndTime])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[dbo].[CustomerHistory], DATA_CONSISTENCY_CHECK=ON));
+
---
+++ ZaawansowaneBazyDanych/dbo/XMLSchemaCollections/ProductAttributeSchema.sql
@@ -0,0 +1,18 @@
+﻿CREATE XML SCHEMA COLLECTION [dbo].[ProductAttributeSchema]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="ProductAttributes" type="ProductAttributesType" />
+  <xsd:complexType name="ProductAttributesType">
+    <xsd:complexContent>
+      <xsd:restriction base="xsd:anyType">
+        <xsd:sequence>
+          <xsd:element name="weight" type="xsd:string" />
+          <xsd:element name="color" type="xsd:string" />
+          <xsd:element name="material" type="xsd:string" />
+          <xsd:element name="size" type="xsd:int" />
+          <xsd:element name="warranty" type="xsd:string" />
+        </xsd:sequence>
+      </xsd:restriction>
+    </xsd:complexContent>
+  </xsd:complexType>
+</xsd:schema>';
+
